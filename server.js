e0cf5a7a1a89041aac2d7be0b55e674b74960aec
Abduvokhid25 <eminovabduvohid@Eminovui-MacBookPro.local>
2025-12-27 20:10:25 +0900

    BRR:  building web server

---
+++ server.js
@@ -0,0 +1,50 @@
+console.log("web serverni boshlash")
+
+console.log("Web serverni boshlash");
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+/*let user;
+fs.readFile("database/user.json", "utf8",(err, data) => {
+    if(err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});*/
+
+//1v kirish code
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extension: true}));
+
+//2 session codes
+
+//3 view code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing code
+
+app.post("/create-items", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function(req, res) {
+    res.render("harid");
+});
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+console.log(`The server is running successfully on port: ${PORT}`);
+});
