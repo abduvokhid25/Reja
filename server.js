b431d4611044e2d2aac00941279f0c307a74d7f8
Abduvokhid25 <eminovabduvohid@Eminovui-MacBookPro.local>
2025-12-27 23:19:03 +0900

    feat: develop author portfolio

--- server.js
+++ server.js
@@ -1,50 +1,43 @@
-console.log("web serverni boshlash")
-
-console.log("Web serverni boshlash");
-
+console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
-/*let user;
-fs.readFile("database/user.json", "utf8",(err, data) => {
-    if(err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data)
+let user;
+fs.readFile("./database/user.json", "utf8", (err, data) => {
+    if(err){
+        console.log("Error:", err);
+    }else{
+        user = JSON.parse(data);
     }
-});*/
+});
 
-//1v kirish code
 
+// 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extension: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//2 session codes
-
-//3 view code
-
+// 2: Session code
+// 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4 Routing code
+// 4 Routing code
+app.post("/create-item", (req, res) => {
 
-app.post("/create-items", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "success" });
+})
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
 });
 
-app.get("/author", (req, res) => {
-    res.render("author", { user: user });
-});
-
-app.get("/", function(req, res) {
-    res.render("harid");
-});
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
-console.log(`The server is running successfully on port: ${PORT}`);
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port: ${PORT}`);
 });
